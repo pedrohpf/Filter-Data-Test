9f65f4f9316e83121e2c91667abed8bc81cda348
Pedro <36662762+pedrohpf@users.noreply.github.com>
2022-02-22 18:18:53 +0000

    cleaned up code + more efficient + more dynamic + added settings

--- lol.py
+++ lol.py
@@ -5,12 +5,24 @@ from io import BytesIO
 import requests
 import math
 import os
+import time
+import json
 
 gitMainPath = "https://raw.githubusercontent.com/pedrohpf/Filter-Data-Test/main/"
 data = requests.get(gitMainPath + "db.json").json()
 
+with open("settings.json") as file:
+	settings = json.load(file)
+	settingsTypes = list(data["settings"].keys())
+	settingsType = settingsTypes[settings["selected"]]
+
 filterLevels = data["filterLevels"]
-champColumns = data["champColumns"]
+champColumns = data["settings"][settingsType]["champColumns"]
+windowSize = data["settings"][settingsType]["windowSize"]
+champProfilesResizeArr = data["settings"][settingsType]["champProfilesResize"]
+champProfilesResize = (champProfilesResizeArr[0], champProfilesResizeArr[1])
+champProfilesExt = data["settings"][settingsType]["champProfilesExt"]
+champProfilesFolder = data["settings"][settingsType]["champProfilesFolder"]
 
 filters = data["filters"]
 teamAttributes = data["teamAttributes"]
@@ -28,11 +40,9 @@ synergyChampLabels = []
 chosenChampNames = []
 chosenChampLabels = []
 champLabels = {}
-
-champProfilesFolder = "champProfiles/"
 os.makedirs(champProfilesFolder, exist_ok=True)
 for i in range(len(champNames)):
-	path = champProfilesFolder + champNames[i] + ".png"
+	path = champProfilesFolder + champNames[i] + champProfilesExt
 	if not os.path.exists(path):
 		champProfile = requests.get(gitMainPath + path).content
 		with open(path, "wb") as handler:
@@ -150,46 +160,60 @@ def updateFilteredChamps():
 		placeChamp(champLabels[filteredChamps[i]], int(i/champColumns), i%champColumns)
 		showChamp(champLabels[filteredChamps[i]], champLabels[filteredChamps[i]].champProfile, champLabels[filteredChamps[i]].champName)
 
-def displayLevelBar(root, i):
-	filterTypeLabel = Label(root, text=teamAttributesTypes[i])
-	filterTypeLabel.grid(row = i, column = 1)
-
-	levelBar = Progressbar(root, orient = HORIZONTAL, length = 100, mode = 'determinate')
-	levelBar.grid(row = i, column = 2)
-
-	return levelBar
-
 def filterNameChange(searchInput):
 	nameFilters[0] = searchInput.get().replace(" ", "")
 	updateFilteredChamps()
 
-def displaySynergies(root):
-	goodVsLabel = Label(root, text="Good Vs")
-	goodVsLabel.grid(row = 1, column = 0, sticky="nsew")
-	badVsLabel = Label(root, text="Bad Vs")
-	badVsLabel.grid(row = 1, column = 1, sticky="nsew")
-	goodWithLabel = Label(root, text="Good With")
-	goodWithLabel.grid(row = 1, column = 2, sticky="nsew")
+#####################################################################################################################
 
-def displaySearch(root):
-	searchInput = StringVar()
+def displayRoot():
+	root = Tk()
+	root.title("Champion Filter")
+	root.geometry(windowSize)
 
-	searchLabel = Label(root, text="Search:")
-	searchLabel.grid(row = 0, column = 0, sticky="nsew")
-	searchBar = Entry(root, textvariable = searchInput)
-	searchBar.bind("<KeyRelease>", lambda event: filterNameChange(searchInput))
-	searchBar.grid(row = 0, column = 1, sticky="nsew")
+	root.grid_columnconfigure(0, weight = 1, uniform="column")
+	root.grid_columnconfigure(1, weight = 2, uniform="column")
+	root.grid_columnconfigure(2, weight = 1, uniform="column")
+	root.grid_rowconfigure(0, weight = 1, uniform="row")
+
+	return root
+
+def displaySections(root):
+	leftFrame = Frame(root)
+	leftFrame.grid(row=0, column=0, sticky="nsew")
+	leftFrame.grid_columnconfigure(0, weight = 1, uniform="column")
+	leftFrame.grid_columnconfigure(1, weight = 1, uniform="column")
+	leftFrame.grid_columnconfigure(2, weight = 1, uniform="column")
+	for i in range(len(filterTypes)):
+		leftFrame.grid_rowconfigure(i, weight = 1, uniform="row")
+
+	middleFrame = Frame(root)
+	middleFrame.grid(row=0, column=1, sticky="nsew")
+	middleFrame.grid_columnconfigure(0, weight = 1, uniform="column")
+	middleFrame.grid_rowconfigure(0, weight = 1, uniform="row")
+	middleFrame.grid_rowconfigure(1, weight = 35, uniform="row")
+
+	rightFrame = Frame(root)
+	rightFrame.grid(row=0, column=2, sticky="nsew")
+	rightFrame.grid_columnconfigure(0, weight = 1, uniform="column")
+	rightFrame.grid_rowconfigure(0, weight = 3, uniform="row")
+	rightFrame.grid_rowconfigure(1, weight = 6, uniform="row")
+	rightFrame.grid_rowconfigure(2, weight = 1, uniform="row")
+	rightFrame.grid_rowconfigure(3, weight = 10, uniform="row")
+
+	return leftFrame, middleFrame, rightFrame
 
 def displayFilter(root, i):
-	filterTypeLabel = Label(root, text=filterTypes[i])
-
 	filterLevelDescription = StringVar()
+	filterLevelDescription.set(filterLevels[0])
 
 	def filterLevelChange(scaleValue):
 		setFilter(filterTypes[i], int(scaleValue))
 		filterLevelDescription.set(filterLevels[int(scaleValue)])
 		updateFilteredChamps()
-	filterLevelChange(0)
+
+	filterTypeLabel = Label(root, text=filterTypes[i])
+	filterTypeLabel.grid(row = i, column = 0)
 
 	filterLevelScale = Scale(root,  from_ = 0,
 									to = len(filterLevels)-1,
@@ -198,100 +222,92 @@ def displayFilter(root, i):
 									showvalue = 0,
 									orient = HORIZONTAL,
 									command = filterLevelChange)
+	filterLevelScale.grid(row = i, column = 1)
 
 	filterLevelLabel = Label(root, textvariable=filterLevelDescription)
-
-	filterTypeLabel.grid(row = i, column = 0)
-	filterLevelScale.grid(row = i, column = 1)
 	filterLevelLabel.grid(row = i, column = 2)
 
-#High need of cleaning up, but who's gonna do that?
-def display(windowSize):
-	root = Tk()
-	root.title("Champion Filter")
-	root.geometry(windowSize)
-
-	root.grid_columnconfigure(0, weight = 1, uniform='column')
-	root.grid_columnconfigure(1, weight = 2, uniform='column')
-	root.grid_columnconfigure(2, weight = 1, uniform='column')
-	root.grid_rowconfigure(0, weight = 1, uniform='row')
-
-	leftFrame = Frame(root)
-	leftFrame.grid(row=0, column=0, sticky="nsew")
-	leftFrame.grid_columnconfigure(0, weight = 1, uniform='column')
-	leftFrame.grid_columnconfigure(1, weight = 1, uniform='column')
-	leftFrame.grid_columnconfigure(2, weight = 1, uniform='column')
+def displayFilters(root):
 	for i in range(len(filterTypes)):
-		leftFrame.grid_rowconfigure(i, weight = 1, uniform='row')
+		displayFilter(root, i)
 
-	middleFrame = Frame(root)
-	middleFrame.grid(row=0, column=1, sticky="nsew")
-	middleFrame.grid_columnconfigure(0, weight = 1, uniform='column')
-	middleFrame.grid_rowconfigure(0, weight = 1, uniform='row')
-	middleFrame.grid_rowconfigure(1, weight = 35, uniform='row')
-
-	rightFrame = Frame(root)
-	rightFrame.grid(row=0, column=2, sticky="nsew")
-	rightFrame.grid_columnconfigure(0, weight = 1, uniform='column')
-	rightFrame.grid_rowconfigure(0, weight = 3, uniform='row')
-	rightFrame.grid_rowconfigure(1, weight = 6, uniform='row')
-	rightFrame.grid_rowconfigure(2, weight = 1, uniform='row')
-	rightFrame.grid_rowconfigure(3, weight = 10, uniform='row')
-
-	searchFrame = Frame(middleFrame)
+def displaySearch(root):
+	searchFrame = Frame(root)
 	searchFrame.grid(row=0, column=0, sticky="nsew")
-	searchFrame.grid_columnconfigure(0, weight = 1, uniform='column')
-	searchFrame.grid_columnconfigure(1, weight = 10, uniform='column')
-	searchFrame.grid_columnconfigure(2, weight = 1, uniform='column')
-	searchFrame.grid_rowconfigure(0, weight = 1, uniform='row')
+	searchFrame.grid_columnconfigure(0, weight = 1, uniform="column")
+	searchFrame.grid_columnconfigure(1, weight = 10, uniform="column")
+	searchFrame.grid_columnconfigure(2, weight = 1, uniform="column")
+	searchFrame.grid_rowconfigure(0, weight = 1, uniform="row")
 
-	champsFrame = Frame(middleFrame)
+	searchInput = StringVar()
+
+	searchLabel = Label(searchFrame, text="Search:")
+	searchLabel.grid(row = 0, column = 0, sticky="nsew")
+	searchBar = Entry(searchFrame, textvariable = searchInput)
+	searchBar.bind("<KeyRelease>", lambda event: filterNameChange(searchInput))
+	searchBar.grid(row = 0, column = 1, sticky="nsew")
+
+def displayChamps(root):
+	champsFrame = Frame(root)
 	champsFrame.grid(row=1, column=0, sticky="nsew")
 	for i in range(champColumns):
-		champsFrame.grid_columnconfigure(i, weight = 1, uniform='column')
+		champsFrame.grid_columnconfigure(i, weight = 1, uniform="column")
 	for i in range(math.ceil(len(champNames)/champColumns)):
-		champsFrame.grid_rowconfigure(i, weight = 1, uniform='row')
-
-	teamFrame = Frame(rightFrame)
-	teamFrame.grid(row=0, column=0, sticky="nsew")
-	teamFrame.grid_columnconfigure(0, weight = 1, uniform='column')
-	teamFrame.grid_columnconfigure(1, weight = 1, uniform='column')
-	teamFrame.grid_columnconfigure(2, weight = 1, uniform='column')
-	teamFrame.grid_columnconfigure(3, weight = 1, uniform='column')
-	teamFrame.grid_columnconfigure(4, weight = 1, uniform='column')
-	teamFrame.grid_rowconfigure(0, weight = 1, uniform='row')
-
-	levelsFrame = Frame(rightFrame)
-	levelsFrame.grid(row=1, column=0, sticky="nsew")
-	levelsFrame.grid_columnconfigure(0, weight = 10, uniform='column')
-	levelsFrame.grid_columnconfigure(1, weight = 13, uniform='column')
-	levelsFrame.grid_columnconfigure(2, weight = 13, uniform='column')
-	levelsFrame.grid_columnconfigure(3, weight = 10, uniform='column')
-	for i in range(len(teamAttributesTypes)):
-		levelsFrame.grid_rowconfigure(i, weight = 1, uniform='row')
-
-	synergiesFrame = Frame(rightFrame)
-	synergiesFrame.grid(row=3, column=0, sticky="nsew")
-	synergiesFrame.grid_columnconfigure(0, weight = 1, uniform='column')
-	synergiesFrame.grid_columnconfigure(1, weight = 1, uniform='column')
-	synergiesFrame.grid_columnconfigure(2, weight = 1, uniform='column')
-	for i in range(8):
-		synergiesFrame.grid_rowconfigure(i, weight = 1, uniform='row')
-
-	for i in range(len(champNames)):
-		path = champProfilesFolder + champNames[i] + ".png"
-		image = Image.open(path).resize((54, 54))
-		champProfiles[champNames[i]] = ImageTk.PhotoImage(image)
+		champsFrame.grid_rowconfigure(i, weight = 1, uniform="row")
 
 	for i in range(len(champNames)):
 		champLabel = createChamp(champsFrame, int(i/champColumns), i%champColumns, champProfile = champProfiles[champNames[i]], champName = champNames[i])
 		champLabel.bind("<Button-1>", lambda event: clickChamp(event))
 		champLabels[champNames[i]] = champLabel
 
+def displayTeams(root):
+	teamFrame = Frame(root)
+	teamFrame.grid(row=0, column=0, sticky="nsew")
+	teamFrame.grid_columnconfigure(0, weight = 1, uniform="column")
+	teamFrame.grid_columnconfigure(1, weight = 1, uniform="column")
+	teamFrame.grid_columnconfigure(2, weight = 1, uniform="column")
+	teamFrame.grid_columnconfigure(3, weight = 1, uniform="column")
+	teamFrame.grid_columnconfigure(4, weight = 1, uniform="column")
+	teamFrame.grid_rowconfigure(0, weight = 1, uniform="row")
+
 	for i in range(5):
 		chosenChampLabel = createChamp(teamFrame, 0, i)
 		chosenChampLabel.bind("<Button-1>", lambda event: clickChosenChamp(event))
 		chosenChampLabels.append(chosenChampLabel)
+
+def displayLevels(root):
+	levelsFrame = Frame(root)
+	levelsFrame.grid(row=1, column=0, sticky="nsew")
+	levelsFrame.grid_columnconfigure(0, weight = 10, uniform="column")
+	levelsFrame.grid_columnconfigure(1, weight = 13, uniform="column")
+	levelsFrame.grid_columnconfigure(2, weight = 13, uniform="column")
+	levelsFrame.grid_columnconfigure(3, weight = 10, uniform="column")
+	for i in range(len(teamAttributesTypes)):
+		levelsFrame.grid_rowconfigure(i, weight = 1, uniform="row")
+
+	for i in range(len(teamAttributesTypes)):
+		filterTypeLabel = Label(levelsFrame, text=teamAttributesTypes[i])
+		filterTypeLabel.grid(row = i, column = 1)
+
+		levelBar = Progressbar(levelsFrame, orient = HORIZONTAL, length = 100, mode = 'determinate')
+		levelBar.grid(row = i, column = 2)
+		levelBars[teamAttributesTypes[i]] = levelBar
+
+def displaySynergies(root):
+	synergiesFrame = Frame(root)
+	synergiesFrame.grid(row=3, column=0, sticky="nsew")
+	synergiesFrame.grid_columnconfigure(0, weight = 1, uniform="column")
+	synergiesFrame.grid_columnconfigure(1, weight = 1, uniform="column")
+	synergiesFrame.grid_columnconfigure(2, weight = 1, uniform="column")
+	for i in range(8):
+		synergiesFrame.grid_rowconfigure(i, weight = 1, uniform="row")
+
+	goodVsLabel = Label(synergiesFrame, text="Good Vs")
+	goodVsLabel.grid(row = 1, column = 0, sticky="nsew")
+	badVsLabel = Label(synergiesFrame, text="Bad Vs")
+	badVsLabel.grid(row = 1, column = 1, sticky="nsew")
+	goodWithLabel = Label(synergiesFrame, text="Good With")
+	goodWithLabel.grid(row = 1, column = 2, sticky="nsew")
 
 	synergyChampLabelSelected = createChamp(synergiesFrame, 0, 1)
 	synergyChampLabelSelected.bind("<Button-1>", lambda event: clickChosenChamp(event))
@@ -302,15 +318,30 @@ def display(windowSize):
 			synergyChampLabelsType.append(createChamp(synergiesFrame, j+2, i))
 		synergyChampLabels.append(synergyChampLabelsType)
 
-	for i in range(len(teamAttributesTypes)):
-		levelBar = displayLevelBar(levelsFrame, i)
-		levelBars[teamAttributesTypes[i]] = levelBar
+def loadChampProfiles():
+	for i in range(len(champNames)):
+		path = champProfilesFolder + champNames[i] + champProfilesExt
+		image = Image.open(path).resize(champProfilesResize)
+		champProfiles[champNames[i]] = ImageTk.PhotoImage(image)
 
-	displaySearch(searchFrame)
-	displaySynergies(synergiesFrame)
-	for i in range(len(filterTypes)):
-		displayFilter(leftFrame, i)
+def display():
+	start = time.perf_counter()
+	root = displayRoot()
+
+	leftFrame, middleFrame, rightFrame = displaySections(root)
+	loadChampProfiles()
+
+	displayFilters(leftFrame)
+
+	displaySearch(middleFrame)
+	displayChamps(middleFrame)
+
+	displayTeams(rightFrame)
+	displayLevels(rightFrame)
+	displaySynergies(rightFrame)
+	end = time.perf_counter()
+	print(end - start)
 
 	root.mainloop()
 
-display("1800x1000")
+display()
