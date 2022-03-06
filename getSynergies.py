3645f0bc7aa3c811eb0a7761e00a52e1b03244cf
Pedro <36662762+pedrohpf@users.noreply.github.com>
2022-03-06 22:03:48 +0000

    Update getSynergies.py

--- getSynergies.py
+++ getSynergies.py
@@ -1,18 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from champs import *
+import requests
 import re
 import time
 
-champNames = list(champs.keys())
+gitMainPath = "https://raw.githubusercontent.com/pedrohpf/Filter-Data-Test/main/"
+data = requests.get(gitMainPath + "db.json").json()
+champs = data["champs"]
+
+patch = "12_4"
 
 chromeService = Service(ChromeDriverManager().install())
 browser = webdriver.Chrome(service=chromeService)
 
+champNames = list(champs.keys())
 synergies = {}
 for champName in champNames:
-	goodvsUrl = "https://u.gg/lol/champions/" + champName + "/matchups?patch=12_4&rank=overall"
+	if champName == "renataglasc":
+		champName = "renata"
+		
+	goodvsUrl = "https://u.gg/lol/champions/" + champName + "/matchups?patch=" + patch + "&rank=overall"
 	browser.get(goodvsUrl)
 	time.sleep(2) #Lazy way of waiting for full load
 	goodvsHtml = browser.page_source
@@ -23,9 +31,11 @@ for champName in champNames:
 		goodvs[i] = goodvs[i].replace(".", "")
 		goodvs[i] = goodvs[i].replace("&amp;", "")
 		goodvs[i] = goodvs[i].replace("Willump", "")
+		goodvs[i] = goodvs[i].replace("Renata", "RenataGlasc")
+		goodvs[i] = goodvs[i].replace("RenataGlascGlasc", "RenataGlasc")
 		goodvs[i] = goodvs[i].lower()
 
-	goodwithUrl = "https://u.gg/lol/champions/" + champName + "/duos?patch=12_4&rank=overall"
+	goodwithUrl = "https://u.gg/lol/champions/" + champName + "/duos?patch=" + patch + "&rank=overall"
 	browser.get(goodwithUrl)
 	time.sleep(2)
 	goodwithHtml = browser.page_source
@@ -36,8 +46,12 @@ for champName in champNames:
 		goodwith[i] = goodwith[i].replace(".", "")
 		goodwith[i] = goodwith[i].replace("&amp;", "")
 		goodwith[i] = goodwith[i].replace("Willump", "")
+		goodwith[i] = goodwith[i].replace("Renata", "RenataGlasc")
+		goodwith[i] = goodwith[i].replace("RenataGlascGlasc", "RenataGlasc")
 		goodwith[i] = goodwith[i].lower()
 
+	if champName == "renata":
+		champName = "renataglasc"
 	synergies[champName] = {"goodvs": goodvs, "goodwith": goodwith}
 
 browser.close()
